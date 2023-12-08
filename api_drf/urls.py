f3b16c39dbc7a72cb36000b2120769dc3fd2177e
Bill Gutierrez <darckneck@hotmail.com>
2023-12-08 00:34:19 -0500

    despliegue10

--- api_drf/urls.py
+++ api_drf/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('proyectos/', include('proyecto.urls')),
+    path('', include('proyecto.urls')),
 ]
